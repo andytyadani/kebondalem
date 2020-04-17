46e93fdf2334f00157975df37b9cfb75aefebc75
andytyadani <63856927+andytyadani@users.noreply.github.com>
2020-04-17 20:04:08 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$email="xxxxxxx@gmail.com";
+$password="xxxxx";
+$user_agent="Mozilla/5.0 (Linux; Android 8.0.0; WAS-LX3 Build/HUAWEIWAS-LX3; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/65.0.3325.109 Mobile Safari/537.36";
+
+?>
